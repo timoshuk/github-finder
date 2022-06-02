4cf289278d640d7cd5f93f08279c35f356221301
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2022-06-02 17:00:10 +0300

    refactoring GithubContext code

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -5,10 +5,13 @@ export const githubReducer = (state, action) => {
     }
     case "GET_USERS":
       return { ...state, users: action.payload, loading: false };
-    case "GET_USER":
-      return { ...state, user: action.payload, loading: false };
-    case "GET_REPOS":
-      return { ...state, repos: action.payload, loading: false };
+    case "GET_USER_AND_REPOS":
+      return {
+        ...state,
+        user: action.payload.user,
+        repos: action.payload.repos,
+        loading: false,
+      };
     case "SET_LOADING":
       return { ...state, loading: true };
     default:
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -6,15 +6,23 @@ import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
 
 import GithubContext from "../context/github/GithubContext";
 
+import { getUserAndRepos } from "../context/github/GithubActions";
+
 const User = () => {
-  const { getUser, user, loading, getUserRepos, repos } =
-    useContext(GithubContext);
+  const { user, loading, repos, dispatch } = useContext(GithubContext);
+
   const params = useParams();
 
   useEffect(() => {
-    getUser(params.login);
-    getUserRepos(params.login);
-  }, []);
+    dispatch({ type: "SET_LOADING" });
+    const getUserData = async () => {
+      const userData = await getUserAndRepos(params.login);
+
+      dispatch({ type: "GET_USER_AND_REPOS", payload: userData });
+    };
+
+    getUserData();
+  }, [dispatch, params.login]);
 
   const {
     name,
